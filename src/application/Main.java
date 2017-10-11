8de581f2bb794e5efd170030a57a6871b1d04374
MHltd <mehrhosse@gmail.com>
2017-10-11 23:48:21 +0200

    Basic styling, merge required

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 		try {
 			//Parent  root = new BorderPane();
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,1280,800);
+			Scene scene = new Scene(root,1289,800);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("DiagSim");
 			primaryStage.setScene(scene);
