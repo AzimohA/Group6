db8e9a4ecb4166bf9e51b569d5a20984515c5884
PILOT <andrewazimoh@gmail.com>
2017-10-23 14:10:37 +0200

    made changes to the main, controller
    merge fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,88 @@
 package application;
 	
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 //allows us to inherit all the javafx funtionlaity into this class
 public class Main extends Application {
+	public static final int PORT = 2222;
 	public static void main(String[] args) {
-		launch(args);
+		//new Main().runServer();
+	    launch(args);
+	}
+	public void runServer()throws IOException{
+	
+		ServerSocket serverSocket = new ServerSocket(PORT);
+	    System.out.println("open for connection ...");
+	  
+	    //accept connection from different clients
+	  
+	    while (true) {
+			Socket socket = serverSocket.accept();
+			new ServerThread(socket).start();
+		}
+	}
+		
+	public class ServerThread extends Thread {
+		Socket socket;
+		ServerThread(Socket socket){
+			this.socket = socket;
+		}
+		
+		public void run(){
+			//read clients input
+			try {
+				String message = null;
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				System.out.println("user "+bufferedReader.readLine()+""+" is now conneceted to the server");
+				//keep reading while the client is sending a message
+				while((message = bufferedReader.readLine())!= null){
+					System.out.println("incoming client message: "+message);
+				}
+				socket.close();
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+					
+		}
 	}
 	@Override
+	
 	public void start(Stage primaryStage) {
 		try {
 			//Parent  root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			//Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+			AnchorPane pane1 = loader.load();
+			MainController mainController = loader.getController();
+			mainController.setMain(this);
+			Scene scene = new Scene(pane1);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	      
+	      
+		 
 			primaryStage.setTitle("ui");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			//new Main().runServer();
+		
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,29 +1,139 @@
 package application;
 
+import java.awt.Rectangle;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.FileChooser;
 
-public class MainController {
+public class MainController implements Initializable{
 	@FXML
 	private Label myRan;
 	@FXML
 	private Button load;
 	@FXML
 	private TextArea textArea;
+	@FXML
+	private Pane pane;
+	@FXML
+	private Button draw;
+	//Pane pane = new Pane();
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 	
+		Canvas canvas = new Canvas(400,400); 
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+       
+        pane.getChildren().add(canvas);
+        pane.setStyle("-fx-background-color: white;");
+        
+		// TODO Auto-generated method stub
+		
+		//group.getChildren().add(new Circle(0, 0, 10));
+		
+	for (int i = 0; i <600; i+=10)
+		{
+		    Line line1 = new Line(i, 0, i, 600);
+		    line1.setStroke(Color.LIGHTGREY);
+		    Line line2 = new Line(0, i, 600, i);
+		    line2.setStroke(Color.LIGHTGREY);
+		   // pane.setStyle("-fx-background-color: white;");
+		   // pane.getChildren().addAll(line1, line2);
+		    pane.getChildren().add(line1);
+		    pane.getChildren().add(line2);
+		    //drawObjects(gc);
+		
+		}
+	canvas.toFront();
+	 drawObjects(gc);
+		
+	  //life lines
+	/*
+	Rectangle r2 = new Rectangle(250,25,100,140);
+	r2.setStroke(Color.BLACK);
+	r2.setFill(null);
+	r2.setStrokeWidth(3);
+	r2.setArcWidth(25);
+	r2.setArcHeight(25);
+	// pane.getChildren().add(r2);
+   */
+	
+	
+	}
+	
+	//@SuppressWarnings("unused")
+	private void drawObjects(GraphicsContext gc) {
+		gc.setStroke(Color.BLACK);
+		gc.setFill(Color.GREEN);
+		gc.strokeRoundRect(160, 60, 40, 30, 10, 10);
+		gc.fillRoundRect(100, 60, 40, 30, 10, 10);
+	
+        gc.setLineWidth(3);
+        gc.strokeLine(120, 90,120, 200);
+        gc.strokeLine(180, 90,180, 200);
+      //  gc.StrokeDashArray(2d);
+        //(x,start,y,length)
+    
+		
+		
+
+		
+	}
+	
+	
+	
+	
+	
+	private Main main;
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	
+
+	 public void drawLine(ActionEvent event) {
+		
+		  
+			
+				for (int i = 0; i <600; i+=10)
+					{
+					    Line line1 = new Line(i, 0, i, 600);
+					    line1.setStroke(Color.LIGHTGREY);
+					    Line line2 = new Line(0, i, 600, i);
+					    line2.setStroke(Color.LIGHTGREY);
+					    pane.setStyle("-fx-background-color: white;");
+					    pane.getChildren().addAll(line1, line2);
+					   
+					}
+			
+	
+	  }
+	 
+	
+    
+    
  public void generateRandom(ActionEvent event){
 	 Random ran = new Random();
 	 int rannum = ran.nextInt(50)+1;
 	 myRan.setText(Integer.toString(rannum));
+	 
 	 //System.out.println(Integer.toString(rannum));
  }
  
@@ -33,8 +143,9 @@ public class MainController {
 	//Set extension filter
 	// File f = fchooser.showOpenDialog(null);
 	// String filename = f.getAbsolutePath(); 
-     FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-     fchooser.getExtensionFilters().add(extFilter);
+   //  FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+	 FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JSON files (*.json)", "*.json");
+	 fchooser.getExtensionFilters().add(extFilter);
      
      try{
     	// FileReader reader = new FileReader(filename);
@@ -43,6 +154,7 @@ public class MainController {
     	File file = fchooser.showOpenDialog(null);
     	if(file != null){
            textArea.setText(readFile(file));
+          
         }
     	 
     	 
@@ -51,6 +163,7 @@ public class MainController {
      }
       
  }
+
 private String readFile(File file){
 	StringBuilder stringBuff = new StringBuilder();
 	BufferedReader br = null;
@@ -71,5 +184,11 @@ private String readFile(File file){
    // }
 	return stringBuff.toString();
  }
+
+
+
  
+ 
+ 
+
 }
